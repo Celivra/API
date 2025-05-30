08b4ca9df944377c2f7e51a6377980917eef2a15
Celivra <52xiaozhai@gmail.com>
2025-05-30 23:53:16 +0800

    add their two controllers

---
+++ src/main/java/com/celivra/api/Controller/ProduceController.java
@@ -0,0 +1,12 @@
+package com.celivra.api.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/produce")
+public class ProduceController {
+
+}
